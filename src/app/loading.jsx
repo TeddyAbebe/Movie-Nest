5a0eae6030ae530e754e10d8e1dae778cc1f89dd
Tewodros Abebe <teddyab729@gmail.com>
2024-03-28 15:10:52 +0300

    Add Movie Page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,13 @@ import React from "react";
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <Image className="h-52" src="spinner.svg" alt="Loading..." />
+      <Image
+        className="h-52"
+        src="spinner.svg"
+        alt="Loading..."
+        width={20}
+        height={20}
+      />
     </div>
   );
 }
