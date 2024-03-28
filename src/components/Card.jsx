f31e7a2f735a81910e52bdc1ce209a1a231a084c
Tewodros Abebe <teddyab729@gmail.com>
2024-03-28 14:31:41 +0300

    Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaRegThumbsUp } from "react-icons/fa";
 
 export default function Card({ result }) {
   return (
-    <div className="group">
+    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
@@ -22,7 +23,11 @@ export default function Card({ result }) {
             {result.title || result.name}
           </h2>
 
-          <p>{result.release_date || result.first_air_date}</p>
+          <p className="flex items-center">
+            {result.release_date || result.first_air_date}
+            <FaRegThumbsUp className="h-5 mr-1 ml-3" />
+            {result.vote_count}
+          </p>
         </div>
       </Link>
     </div>
